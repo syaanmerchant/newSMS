d49bc76541f2bcb0872ff68020ccfde0794391d3
mahnoornav <mahnoornaveed06@gmail.com>
2025-11-25 20:25:20 -0500

    logic for adding medication to the list

--- src/core.c
+++ src/core.c
@@ -11,3 +11,27 @@ void init_med_list(MedicationList *list) {
     // list sets the ID number that will be assigned to the next medication added
     list->next_id = 1;
 }
+
+void add_medication(MedicationList *list, Medication m) {
+    if (!list) return;
+
+    // if the user reaches the max medications amount then return the list 
+    if (list->count >= MAX_MEDICATIONS) return;
+
+    // sets medication id
+    m.id = list->next_id++;
+
+    // if the recurrence is invalid and doesn't match daily or once, then default it to daily
+    // NOTE: maybe there should be a warning first
+    if (m.recurrence != RECURRENCE_DAILY && m.recurrence != RECURRENCE_ONCE) {
+        m.recurrence = RECURRENCE_DAILY;
+    }
+
+    // if the medicine is one time use, reset the flag to 0
+    if (m.recurrence == RECURRENCE_ONCE && m.one_time_used != 0) {
+        m.one_time_used = 0;
+    }
+
+    // stores medication in list
+    list->meds[list->count++] = m;
+}
